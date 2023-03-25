540ba7334056ef347cb31f158b6ef16540ebdaa7
AKP-13 <alexpeirson@hotmail.com>
2023-03-25 16:54:35 +0000

    Code sytax refactor. Add totals row.

--- src/savingsProjections/SavingsProjectionsTable.js
+++ src/savingsProjections/SavingsProjectionsTable.js
@@ -1,81 +1,74 @@
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
 
 const columns = [
-  'January 2023',
-  'February 2023',
-  'March 2023',
-  'April 2023',
-  'May 2023',
-  'June 2023',
-  'July 2023',
-  'August 2023',
-  'September 2023',
-  'October 2023',
-  'November 2023',
-  'December 2023'
+  { month: 'January 2023', saved: 100, withdrawn: 0 },
+  { month: 'February 2023', saved: 100, withdrawn: 0 },
+  { month: 'March 2023', saved: 100, withdrawn: 100 },
+  { month: 'April 2023', saved: 300, withdrawn: 0 },
+  { month: 'May 2023', saved: 100, withdrawn: 0 },
+  { month: 'June 2023', saved: 100, withdrawn: 0 },
+  { month: 'July 2023', saved: 100, withdrawn: 50 },
+  { month: 'August 2023', saved: 100, withdrawn: 0 },
+  { month: 'September 2023', saved: 100, withdrawn: 0 },
+  { month: 'October 2023', saved: 100, withdrawn: 0 },
+  { month: 'November 2023', saved: 100, withdrawn: 200 },
+  { month: 'December 2023', saved: 100, withdrawn: 0 },
+  { month: 'January 2024', saved: 100, withdrawn: 0 },
+  { month: 'February 2024', saved: 100, withdrawn: 0 },
+  { month: 'March 2024', saved: 100, withdrawn: 100 },
+  { month: 'April 2024', saved: 300, withdrawn: 0 },
+  { month: 'May 2024', saved: 100, withdrawn: 0 },
+  { month: 'June 2024', saved: 100, withdrawn: 0 },
+  { month: 'July 2024', saved: 100, withdrawn: 50 },
+  { month: 'August 2024', saved: 100, withdrawn: 0 },
+  { month: 'September 2024', saved: 100, withdrawn: 0 },
+  { month: 'October 2024', saved: 100, withdrawn: 0 },
+  { month: 'November 2024', saved: 100, withdrawn: 200 },
+  { month: 'December 2024', saved: 100, withdrawn: 0 },
+  { month: 'January 2025', saved: 100, withdrawn: 0 },
+  { month: 'February 2025', saved: 100, withdrawn: 0 },
+  { month: 'March 2025', saved: 100, withdrawn: 100 },
+  { month: 'April 2025', saved: 300, withdrawn: 0 },
+  { month: 'May 2025', saved: 100, withdrawn: 0 },
+  { month: 'June 2025', saved: 100, withdrawn: 0 },
+  { month: 'July 2025', saved: 100, withdrawn: 50 },
+  { month: 'August 2025', saved: 100, withdrawn: 0 },
+  { month: 'September 2025', saved: 100, withdrawn: 0 },
+  { month: 'October 2025', saved: 100, withdrawn: 0 },
+  { month: 'November 2025', saved: 100, withdrawn: 200 },
+  { month: 'December 2025', saved: 100, withdrawn: 0 }
 ];
 
+let accumulatedSaved = 0;
+let accumulatedWithdrawn = 0;
+
+const totalSaved = columns.map(({ month, saved, withdrawn }) => {
+  accumulatedSaved += saved;
+  accumulatedWithdrawn += withdrawn;
+
+  const amount = accumulatedSaved - accumulatedWithdrawn;
+
+  return { month, amount };
+});
+
 export default function SavingsProjectionsTable() {
-  const saved = [
-    { month: 'January 2023', amount: 100 },
-    { month: 'February 2023', amount: 100 },
-    { month: 'March 2023', amount: 100 },
-    { month: 'April 2023', amount: 300 },
-    { month: 'May 2023', amount: 100 },
-    { month: 'June 2023', amount: 100 },
-    { month: 'July 2023', amount: 100 },
-    { month: 'August 2023', amount: 100 },
-    { month: 'September 2023', amount: 100 },
-    { month: 'October 2023', amount: 100 },
-    { month: 'November 2023', amount: 100 },
-    { month: 'December 2023', amount: 100 }
-  ];
-
-  const withdrawn = [
-    { month: 'January 2023', amount: 0 },
-    { month: 'February 2023', amount: 0 },
-    { month: 'March 2023', amount: 100 },
-    { month: 'April 2023', amount: 0 },
-    { month: 'May 2023', amount: 0 },
-    { month: 'June 2023', amount: 0 },
-    { month: 'July 2023', amount: 50 },
-    { month: 'August 2023', amount: 0 },
-    { month: 'September 2023', amount: 0 },
-    { month: 'October 2023', amount: 0 },
-    { month: 'November 2023', amount: 200 },
-    { month: 'December 2023', amount: 0 }
-  ];
-
-  const total = [
-    { month: 'January 2023', amount: 100 },
-    { month: 'February 2023', amount: 200 },
-    { month: 'March 2023', amount: 200 },
-    { month: 'April 2023', amount: 500 },
-    { month: 'May 2023', amount: 600 },
-    { month: 'June 2023', amount: 700 },
-    { month: 'July 2023', amount: 750 },
-    { month: 'August 2023', amount: 850 },
-    { month: 'September 2023', amount: 950 },
-    { month: 'October 2023', amount: 1050 },
-    { month: 'November 2023', amount: 950 },
-    { month: 'December 2023', amount: 1050 }
-  ];
-
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             <TableCell />
-            {columns.map((col) => (
-              <TableCell key={col}>{col}</TableCell>
+            {columns.map(({ month }) => (
+              <TableCell key={month}>{month}</TableCell>
             ))}
           </TableRow>
         </TableHead>
@@ -84,28 +77,27 @@ export default function SavingsProjectionsTable() {
             <TableCell component="th" scope="row">
               Saved
             </TableCell>
-            {columns.map((col) => {
-              const value = saved.find((obj) => obj.month === col).amount;
-              return <TableCell key={col}>{value}</TableCell>;
-            })}
+            {columns.map(({ month, saved }) => (
+              <TableCell key={`${month}-saved`}>{saved}</TableCell>
+            ))}
           </TableRow>
+
           <TableRow key="Withdrawn" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
             <TableCell component="th" scope="row">
               Withdrawn
             </TableCell>
-            {columns.map((col) => {
-              const value = withdrawn.find((obj) => obj.month === col).amount;
-              return <TableCell key={col}>{value}</TableCell>;
-            })}
+            {columns.map(({ month, withdrawn }) => (
+              <TableCell key={`${month}-withdrawn`}>{withdrawn}</TableCell>
+            ))}
           </TableRow>
+
           <TableRow key="Total" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
             <TableCell component="th" scope="row">
               Total
             </TableCell>
-            {columns.map((col) => {
-              const value = total.find((obj) => obj.month === col).amount;
-              return <TableCell key={col}>{value}</TableCell>;
-            })}
+            {totalSaved.map(({ month, amount }) => (
+              <TableCell key={`${month}-total`}>{amount}</TableCell>
+            ))}
           </TableRow>
         </TableBody>
       </Table>
