5138de5152d38c10fc0684838e8fff5ec6cb6675
AKP-13 <alexpeirson@hotmail.com>
2023-04-04 13:03:52 +0100

    Add sticky column. Refactor table styling

--- src/savingsProjections/SavingsProjectionsTable.js
+++ src/savingsProjections/SavingsProjectionsTable.js
@@ -1,25 +1,38 @@
 /* eslint-disable react/prop-types */
 import React, { useMemo } from 'react';
 import {
+  Paper,
+  styled,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
-  TableRow,
-  Paper
+  TableRow
 } from '@mui/material';
 import { formattedCurrency, returnTotals } from './helpers';
 
 export default function SavingsProjectionsTable({ tableData }) {
   const totalSaved = useMemo(() => returnTotals({ tableData }), [tableData]);
 
+  const StickyCell = styled(TableCell)(() => ({
+    position: 'sticky',
+    left: 0,
+    background: 'white'
+  }));
+
+  const StyledTableRow = styled(TableRow)(() => ({
+    [`&:last-child td, &:last-child th`]: {
+      border: 0
+    }
+  }));
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell />
+            <StickyCell />
             {tableData.map(({ month }) => (
               <TableCell key={month} sx={{ minWidth: '100px' }}>
                 {month}
@@ -28,36 +41,36 @@ export default function SavingsProjectionsTable({ tableData }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow key="Saved" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell component="th" scope="row">
+          <StyledTableRow key="Saved">
+            <StickyCell component="th" scope="row">
               Saved
-            </TableCell>
+            </StickyCell>
             {tableData.map(({ month, saved }) => (
               <TableCell key={`${month}-saved`}>{`+${formattedCurrency.format(saved)}`}</TableCell>
             ))}
-          </TableRow>
+          </StyledTableRow>
 
-          <TableRow key="Withdrawn" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell component="th" scope="row">
+          <StyledTableRow key="Withdrawn">
+            <StickyCell component="th" scope="row">
               Withdrawn
-            </TableCell>
+            </StickyCell>
             {tableData.map(({ month, withdrawn }) => (
               <TableCell key={`${month}-withdrawn`}>
                 {withdrawn === 0 ? '' : `-${formattedCurrency.format(withdrawn)}`}
               </TableCell>
             ))}
-          </TableRow>
+          </StyledTableRow>
 
-          <TableRow key="Total" sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell component="th" scope="row">
+          <StyledTableRow key="Total">
+            <StickyCell component="th" scope="row">
               Total
-            </TableCell>
+            </StickyCell>
             {totalSaved.map(({ month, amount }) => (
               <TableCell
                 sx={{ backgroundColor: amount === 0 ? 'grey' : amount < 0 ? 'red' : 'green' }}
                 key={`${month}-total`}>{`${formattedCurrency.format(amount)}`}</TableCell>
             ))}
-          </TableRow>
+          </StyledTableRow>
         </TableBody>
       </Table>
     </TableContainer>
